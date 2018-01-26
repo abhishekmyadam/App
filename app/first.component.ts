e15f92ab779510b0d3d6c398a3f53b5f07ed42ab
abhishekmyadam <myadamabhishek@gmail.com>
2018-01-25 23:10:11 -0800

    firsst

--- app/first.component.ts
+++ app/first.component.ts
@@ -3,7 +3,7 @@ import{component} from '@angular/core';
 @Component({
 
 selector:'first-app',
-template:'<h2>First Application component </h2>'
+template:'<h2>First Application components </h2>'
 })
 
 export class FirstComponent{
