37e1cf7c3b6e7764272be2fe4a550d84c94cf215
abhishek <abhishekmyadam@gmail.com>
2018-01-25 23:03:18 -0800

    initial commit

---
+++ app/first.component.ts
@@ -0,0 +1,13 @@
+import{component} from '@angular/core';
+
+@Component({
+
+selector:'first-app',
+template:'<h2>First Application component </h2>'
+})
+
+export class FirstComponent{
+
+
+
+}
---
+++ app/main.js
@@ -0,0 +1,12 @@
+"use strict";
+/*import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var first_component_1 = require("./first.component");
+platform_browser_dynamic_1.bootstrap(first_component_1.FirstComponent);
+//# sourceMappingURL=main.js.map
